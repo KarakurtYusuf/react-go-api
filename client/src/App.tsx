2d46c990a339f9c30384887a4ab0c7c06c669dbd
Yusuf Karakurt <yusufkarakurt0658@gmail.com>
2026-05-12 23:47:58 +0300

    5. gün: Frontend ve Backend entegresi saglandi. Tip (Type) hatalari cozuldu, React Query ayarlandi ve veritabani baglantisi aktif edildi.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import { Container, Stack } from '@chakra-ui/react'
 import Navbar from './components/Navbar'
-import TodoForm from './components/ToDoForm'
-import TodoList from './components/ToDoList'
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
 
 
-
+export const BASE_URL = "http://localhost:5000/api";
 function App() {
 
   return (
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, theme} from '@chakra-ui/react'
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
-      
+    <QueryClientProvider client={queryClient}>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
