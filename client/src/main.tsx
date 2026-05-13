567f11309327ecc0fd6da3f78fa578e8cdd99d28
Yusuf Karakurt <yusufkarakurt0658@gmail.com>
2026-05-13 16:40:24 +0300

    6.gün: Dist klasörü ve final yayını

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ChakraProvider, theme} from '@chakra-ui/react'
+import { ChakraProvider} from '@chakra-ui/react'
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
+import theme from './chakra/theme.ts'
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
